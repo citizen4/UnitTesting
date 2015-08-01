204192b0e9b7f0c362dad31e9987a486ff5fbfd2
citizen4 <citizen4@example.com>
2015-08-01 17:59:53 +0200

    Initial commit

---
+++ src/main/java/citizen4/App.java
@@ -0,0 +1,13 @@
+package citizen4;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
